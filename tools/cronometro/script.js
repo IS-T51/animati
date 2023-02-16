febf1a83a9a2fc6900f9baba00bfb01a24898562
tpanjkovic <tpanjkovic@fbk.eu>
2023-02-16 22:27:51 +0100

    Added fischietto and cronometro

--- tools/cronometro/script.js
+++ tools/cronometro/script.js
@@ -1,1 +1,60 @@
 // Script per la pagina `/tools/cronometro`
+
+$(document).ready(() => {
+    var start
+    var running = false
+    var intervalId
+
+    var startStopButton = $("#startStopButton")
+    var partialButton = $("#partialButton")
+    var counter = $("#counter")
+    var partials = $("#partials")
+    var partialsTitle = $("#partialsTitle")
+
+    function formatTime(millis) {
+        var hundredth = Math.trunc(millis / 10) % 100
+        var seconds = Math.trunc(millis / 1000) % 60
+        var minutes = Math.trunc(millis / 60000)
+
+        hundredth = hundredth < 10 ? "0" + hundredth : hundredth
+        seconds = seconds < 10 ? "0" + seconds : seconds
+        minutes = minutes < 10 ? "0" + minutes : minutes
+
+        return minutes + ":" + seconds + ":" + hundredth
+    }
+    function showTime(millis) {
+        counter.text(formatTime(millis))
+    }
+    function currentTime() {
+        return Date.now() - start
+    }
+
+    startStopButton.on("click", () => {
+        if (running) {
+            running = false;
+            clearInterval(intervalId)
+            showTime(currentTime())
+            startStopButton.text("Avvia")
+            partialButton[0].disabled = true
+            
+        } else {
+            running = true;
+            start = Date.now()
+            startStopButton.text("Ferma")
+            partialButton[0].disabled = false
+            partialsTitle.hide()
+            partials.text("")
+
+            intervalId = setInterval(() => {
+                showTime(currentTime())
+            }, 10)
+        }
+    })
+
+    partialButton.on("click", () => {
+        if (running) {
+            partialsTitle.show()
+            partials.prepend(formatTime(currentTime()) + "<br/>")
+        }
+    })
+})
--- tools/fischietto/script.js
+++ tools/fischietto/script.js
@@ -1,1 +1,41 @@
 // Script per la pagina `/tools/fischietto`
+
+$(document).ready(() =>{
+    let s1 = 'https://soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3'
+    let s2 = 'https://soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3'
+    let s3 = 'https://soundhelix.com/examples/mp3/SoundHelix-Song-3.mp3'
+    
+    let startImage = $('#startImage');
+    let stopButton = $('#stopButton');
+    let dropdown = $('#nome_dropdown');
+
+    let suono;
+
+    function onStop() {
+        dropdown[0].disabled = false
+        startImage.show()
+        stopButton.hide()
+        suono.pause()
+    }
+
+    function setSuono(s, i) {
+        suono = new Audio(s);
+        dropdown.text('Suono ' + i);
+        suono.addEventListener("ended", onStop)
+    }
+
+    setSuono(s1, 1)
+    $('#suono1').on("click", () => setSuono(s1, 1));
+    $('#suono2').on("click", () => setSuono(s2, 2));
+    $('#suono3').on("click", () => setSuono(s3, 3));
+
+    startImage.on("click", () => {
+        dropdown[0].disabled = true
+        startImage.hide()
+        stopButton.show()
+        suono.currentTime = 0
+        suono.play()
+    })
+
+    stopButton.on("click", onStop)
+});
